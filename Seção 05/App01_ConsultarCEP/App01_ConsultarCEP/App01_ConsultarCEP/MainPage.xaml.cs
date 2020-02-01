8f19631311667b6453eef991a0099ab7fb8ccd90
jloureiro7 <jota-loureiro@hotmail.com>
2020-02-01 23:12:24 +0000

    teste 2

--- Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -32,7 +32,6 @@ namespace App01_ConsultarCEP
                 try
                 {
                     Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
-                    
                     if(end != null)
                     {
                         RESULTADO.Text = string.Format("Endereço: {2} de {3} {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro);
